4507ae3f93c417b67f03624f906979896f391300
Wojciech Kubiak <wgkubiak@gmail.com>
2020-03-19 21:15:44 +0100

    Exams placeholders and some positioning

--- src/containers/ExamContainer.js
+++ src/containers/ExamContainer.js
@@ -1,20 +1,123 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
+import { Card, Button } from "react-bootstrap";
 
 const ExamContainer = props => {
 
-    useEffect(() => {
-        console.log(props.unitID)
-    })
+let longtext = "N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A";
 
-    return (
-        <div className="exam">
-            <div className="exam--transparent"></div>
-            <div className="exam--container">
-                <button onClick={props.toggleExams}>SCHOWAJ</button>
-                <h1>{props.unitID}</h1>
-            </div>
-        </div>
-    )
+const showPartOfString = (string, l) => {
+    if(string.length <= l) {
+        return string;
+    }
+        
+    return string.substr(0, string.lastIndexOf(' ', l));
 }
+
+  useEffect(() => {
+    console.log(props.unitID);
+  });
+
+  return (
+    <div className="exam">
+      <div className="exam--transparent"></div>
+      <div className="exam--container">
+        <button onClick={props.toggleExams}>SCHOWAJ</button>
+        <h1>{props.unitID}</h1>
+        <div className="exam--container--cards">
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Odchody</Card.Title>
+              <Card.Text>2020-03-19 | 12:00:00</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Tkanka</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Leki</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Ilość</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Rodzaj leków</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Rozwolnienie</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Waga</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Temperatura</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Kulawizna</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Układ oddechowy</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="exam--container--cards-data exam--result-card-small">
+            <Card.Body>
+              <Card.Title>Zmiany naskórne</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>N/A</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="exam--container--cards-data exam--result-card-wide">
+            <Card.Body>
+              <Card.Title>Wynik egzaminu</Card.Title>
+              <Card.Text>2020-03-19</Card.Text>
+              <Card.Text>
+                {showPartOfString(longtext, 50)} ...
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ExamContainer;
